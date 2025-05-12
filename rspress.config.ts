db0a2730423255e5408ded089fe943ae74dc356c
mikigo <1964191531@qq.com>
2025-05-12 19:26:30 +0800

    update

--- rspress.config.ts
+++ rspress.config.ts
@@ -74,7 +74,7 @@ export default defineConfig({
             {
                 icon: 'github',
                 mode: 'link',
-                content: 'https://github.com/mikigo/sailwind_docs',
+                content: 'https://github.com/mikigo/sailwind_docs/blob/main/about.md',
             }
         ],
 
