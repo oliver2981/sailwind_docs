dfc33018b6e167182262a682c9259052bfb45ee1
mikigo <1964191531@qq.com>
2025-05-29 18:12:40 +0800

    update

--- scripts/add_empty_line.py
+++ scripts/add_empty_line.py
@@ -36,10 +36,10 @@ if __name__ == '__main__':
     root_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
     docs_dir = os.path.join(root_dir, "docs")
 
-    for file in os.listdir(os.path.join(docs_dir, "v4", "logic", "tutorial")):
+    for file in os.listdir(os.path.join(docs_dir, "lpcreator", "guide")):
         if not file.endswith("zh.md"):
             continue
-        md_files = os.path.join(docs_dir, "v4", "logic", "tutorial", file)
+        md_files = os.path.join(docs_dir, "lpcreator", "guide", file)
         with  open(md_files, 'r', encoding='utf-8') as f:
             markdown_text = f.read()
 
